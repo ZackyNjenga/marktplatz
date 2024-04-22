934a11c07a250ecd9b8d651cd82acd58a109a3d5
zacharynjenga381@gmail.com <zacharynjenga381@gmail.com>
2024-04-22 13:01:12 +0300

    A first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+            <header className="relative bg-white">
+                <MaxWidthWrapper>
+                    <div className="border-b border-gray-600">
+                        <div className="flex h-16 items-center">
+                            <div className="ml-4 flex lg:ml-0">
+                                <Link href="/">
+
+                                </Link>
+                            </div>
+                        </div>
+
+                    </div>
+                </MaxWidthWrapper>
+            </header>
+        </div>
+)
+}

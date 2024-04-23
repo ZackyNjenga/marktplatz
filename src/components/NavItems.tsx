d482e5d91af16fa9ae8cd2a8550d1ea6944dd858
zacharynjenga381@gmail.com <zacharynjenga381@gmail.com>
2024-04-23 06:22:57 +0300

    Todays commit

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,54 @@
 "use client"
 
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import {PRODUCT_CATEGORIES} from "@/config";
+import NavItem from "@/components/NavItem";
+import {useOnClickOutside} from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
     const [activeIndex, setActiveindex] = useState<
         null | number
     >(null)
-    return (
-        <div className="flex gap-4 h-full">
 
+    useEffect(() => {
+        const handler = (e: keyboardEvent) => {
+            if(e.key === "Escape") {
+                setActiveindex(null)
+            }
+        }
+        document.addEventListener('keydown', handler)
+
+        return () => {
+            document.removeEventListener('keydown', handler)
+        }
+    }, []);
+    const isAnyOpen = activeIndex !== null
+
+     const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef, () => setActiveindex(null))
+    return (
+        <div className="flex gap-4 h-full" ref={navRef}>
+            {PRODUCT_CATEGORIES.map((category, i ) => {
+                const handleOpen = () => {
+                    if(activeIndex === i) {
+                        setActiveindex(null)
+                    } else {
+                        setActiveindex(i)
+                    }
+                }
+
+                const isOpen = i === activeIndex
+                return (
+                    <NavItem
+                        category={category}
+                        handleOpen={handleOpen}
+                        isOpen={isOpen}
+                        key={category.value}
+                        isAnyOpen={isAnyOpen}
+                    />
+                )
+            })}
         </div>
     )
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,38 @@ export const PRODUCT_CATEGORIES = [
             {
                 name: "Editor's picks",
                 href: '#',
-                imageSrc: '/nav/ui-kits/mixed/jpeg',
+                imageSrc: '/nav/ui-kits/mixed.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: '#',
+                imageSrc: '/nav/ui-kits/blue.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: '#',
+                imageSrc: '/nav/ui-kits/purple.jpg',
+            },
+        ],
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "Fave Icon Picks",
+                href: '#',
+                imageSrc: '/nav/icons/picks.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: '#',
+                imageSrc: '/nav/icons/new.jpg',
+            },
+            {
+                name: "New Arrivals",
+                href: '#',
+                imageSrc: '/nav/icons/bestsellers.jpg',
             },
         ],
     },
